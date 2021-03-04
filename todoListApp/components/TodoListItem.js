df30c1904aa136ded5d5739bf82bcbf24d6616f7
acebogo <koprodev@gmail.com>
2021-03-04 12:56:14 +0900

    TodoListItem

--- todoListApp/components/TodoListItem.js
+++ todoListApp/components/TodoListItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+// react-native link react-native-vector-icons
 
 const TodoListItem = () => {
   return (
     <View style={styles.container}>
       <TouchableOpacity>
-        <View style={styles.circle} />
+        <View style={styles.completeCircle}>
+          <Icon name="circledowno" size={30} color="#3143e8" />
+        </View>
       </TouchableOpacity>
-      <Text style={styles.text}>TodoList items will be shown here</Text>
+      <Text style={[styles.text, styles.strikeText]}>
+        Items will be shown here
+      </Text>
     </View>
   );
 };
@@ -37,6 +43,21 @@ const styles = StyleSheet.create({
     marginRight: 20,
     marginLeft: 20,
   },
+  completeCircle: {
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  strikeText: {
+    color: '#bbb',
+    textDecorationLine: 'line-through',
+  },
+  unstrikeText: {
+    color: '#29323c',
+  },
+  buttonContainer: {
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },  
 });
 
 export default TodoListItem;
