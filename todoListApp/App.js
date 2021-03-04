f7903933730e5f807945bc954e6118aef491a894
acebogo <koprodev@gmail.com>
2021-03-04 12:48:52 +0900

    TodoInsert  컴포넌트 분리

--- todoListApp/App.js
+++ todoListApp/App.js
@@ -8,20 +8,23 @@ import {
   ScrollView,
 } from 'react-native';
 
+import TodoInsert from './components/TodoInsert';
+
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.appTitle}>Hello Todolist</Text>
       <View style={styles.card}>
-        <TextInput style={styles.input} placeholder="Add an item!" />
+        <TodoInsert />
         <ScrollView>
+          <View>
             <Text>TodoList</Text>
+          </View>
         </ScrollView>
       </View>
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
